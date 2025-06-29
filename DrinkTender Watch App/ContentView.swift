72c7f41ccb37fe4c6ab2e87b87e7e753b274ec85
Jeff Trespalacios <jeff@j3p.co>
2025-06-28 17:59:15 -0700

    Initial Commit

---
+++ DrinkTender Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DrinkTender Watch App
+//
+//  Created by Jeff Trespalacios on 6/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DrinkTender Watch App/DrinkTenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkTenderApp.swift
+//  DrinkTender Watch App
+//
+//  Created by Jeff Trespalacios on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct DrinkTender_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
